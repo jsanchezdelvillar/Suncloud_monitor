e9bc6929782dd27edf9c50a3c343e232207c142b
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-05-21 13:43:02 +0200

    Update config_flow.py

--- custom_components/suncloud_monitor/config_flow.py
+++ custom_components/suncloud_monitor/config_flow.py
@@ -1,22 +1,66 @@
-from homeassistant import config_entries
 import voluptuous as vol
-from .const import DOMAIN
+from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.helpers import selector
 
-class SunCloudConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle config flow for SunCloud Monitor."""
+from .const import (
+    DOMAIN,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_APPKEY,
+    CONF_SECRET,
+    CONF_RSA,
+    CONF_BASE_URL,
+    CONF_PS_KEY,
+    CONF_POLL_INTERVAL,
+)
 
+DEFAULT_BASE_URL = "https://gateway.isolarcloud.eu"
+DEFAULT_POLL_INTERVAL = 5
+
+class SuncloudConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="SunCloud Monitor", data=user_input)
+        errors = {}
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("username"): str,
-                vol.Required("password"): str,
-                vol.Required("base_url", default="https://your-api-url.com"): str,
-                vol.Optional("poll_interval", default=5): vol.All(int, vol.Range(min=1, max=60))
-            })
-        )
+        if user_input is not None:
+            return self.async_create_entry(title="Suncloud Monitor", data=user_input)
+
+        schema = vol.Schema({
+            vol.Required(CONF_USERNAME): str,
+            vol.Required(CONF_PASSWORD): str,
+            vol.Required(CONF_APPKEY): str,
+            vol.Required(CONF_SECRET): str,
+            vol.Required(CONF_RSA): str,
+            vol.Required(CONF_PS_KEY): str,
+            vol.Optional(CONF_BASE_URL, default=DEFAULT_BASE_URL): str,
+            vol.Optional(CONF_POLL_INTERVAL, default=DEFAULT_POLL_INTERVAL): int,
+        })
+
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return SuncloudOptionsFlowHandler(config_entry)
+
+
+class SuncloudOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        return await self.async_step_user(user_input)
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        options = self.config_entry.options
+
+        schema = vol.Schema({
+            vol.Optional(CONF_POLL_INTERVAL, default=options.get(CONF_POLL_INTERVAL, DEFAULT_POLL_INTERVAL)): int
+        })
+
+        return self.async_show_form(step_id="user", data_schema=schema)
