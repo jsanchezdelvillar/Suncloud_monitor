45a564a42a8a35f0ade32968ed4d94ea10516c8c
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-05-21 13:41:42 +0200

    Update const.py

--- custom_components/suncloud_monitor/const.py
+++ custom_components/suncloud_monitor/const.py
@@ -1,6 +1,12 @@
+"""Constants for the Suncloud Monitor integration."""
+
 DOMAIN = "suncloud_monitor"
 
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
+CONF_APPKEY = "appkey"
+CONF_SECRET = "sung_secret"
+CONF_RSA = "RSA_public"
 CONF_BASE_URL = "base_url"
+CONF_PS_KEY = "ps_key"
 CONF_POLL_INTERVAL = "poll_interval"
