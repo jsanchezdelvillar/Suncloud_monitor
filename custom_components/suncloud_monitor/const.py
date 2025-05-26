af2709a671c824d66b83a69b5d6502d81d4144bc
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-05-26 14:10:56 +0200

    Update const.py

--- custom_components/suncloud_monitor/const.py
+++ custom_components/suncloud_monitor/const.py
@@ -2,11 +2,22 @@
 
 DOMAIN = "suncloud_monitor"
 
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-CONF_APPKEY = "appkey"
-CONF_SECRET = "sung_secret"
-CONF_RSA = "RSA_public"
-CONF_BASE_URL = "base_url"
-CONF_PS_KEY = "ps_key"
-CONF_POLL_INTERVAL = "poll_interval"
+# Default API Base URL
+API_BASE_URL = "https://gateway.isolarcloud.eu"
+
+# API Endpoints
+LOGIN_ENDPOINT = "/openapi/login"
+PLANT_LIST_ENDPOINT = "/openapi/getPowerStationList"
+DEVICE_LIST_ENDPOINT = "/openapi/getDeviceList"
+PLANT_INFO_ENDPOINT = "/openapi/getPowerStationDetail"
+REALTIME_ENDPOINT = "/openapi/getDeviceRealTimeData"
+POINT_DISCOVERY_ENDPOINT = "/openapi/getOpenPointInfo"
+
+# HA state keys
+TOKEN_KEY = "input_text.token"
+PS_KEY_STATE = "input_text.ps_key"
+PLANT_ID_STATE = "sensor.plant_id"
+METER_SN_STATE = "sensor.meter_sn"
+
+# UI helpers
+TELEMETRY_SELECT = "input_select.telemetry_points"
