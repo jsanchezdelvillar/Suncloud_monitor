8ce3434ff6c559966045f57ef0292ad6f32425d1
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-05-26 13:33:30 +0200

    Update __init__.py

--- custom_components/suncloud_monitor/__init__.py
+++ custom_components/suncloud_monitor/__init__.py
@@ -1,25 +1,23 @@
-"""Suncloud Monitor integration."""
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
-
 async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up integration from configuration.yaml (not used)."""
     return True
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Suncloud Monitor from config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN]["config"] = entry.data
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    return True
 
+    # NEW: Recommended in HA 2025+
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload Suncloud Monitor config entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     if unload_ok:
         hass.data[DOMAIN].pop("config", None)
     return unload_ok
