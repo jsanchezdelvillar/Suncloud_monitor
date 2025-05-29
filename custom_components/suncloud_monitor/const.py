c661ad9b81743c6aeaa6eb2c1ba720ec337dd53f
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-05-29 10:14:28 +0200

    Create const.py

---
+++ custom_components/suncloud_monitor/const.py
@@ -0,0 +1,13 @@
+DOMAIN = "suncloud_monitor"
+PLATFORMS = ["sensor"]
+DEFAULT_SCAN_INTERVAL = 300  # 5 minutes
+
+CONF_APPKEY = "appkey"
+CONF_ACCESS_KEY = "access_key"
+CONF_RSA_KEY = "rsa_key"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+CONF_POINTS = "enabled_points"
+
+CONFIG_STORAGE_FILE = "config_storage.yaml"
