e752610c4f71fae6c9f3d15665a326402bf9de2a
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-06-02 12:45:36 +0200

    Update __init__.py

--- custom_components/suncloud_monitor/__init__.py
+++ custom_components/suncloud_monitor/__init__.py
@@ -7,6 +7,7 @@ from .coordinator import SuncloudDataCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = SuncloudDataCoordinator(hass, entry.data)
     await coordinator.async_config_entry_first_refresh()
@@ -14,11 +15,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = hass.data[DOMAIN].get(entry.entry_id)
     if coordinator:
         await coordinator.async_close()
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry, PLATFORMS
+    )
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
