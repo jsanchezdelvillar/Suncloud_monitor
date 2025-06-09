3a56106632a8fea1500fd7f9c369b452faa5ea17
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-06-09 12:41:36 +0200

    Update __init__.py

--- custom_components/suncloud_monitor/__init__.py
+++ custom_components/suncloud_monitor/__init__.py
@@ -7,19 +7,21 @@ from .coordinator import SuncloudDataCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = SuncloudDataCoordinator(hass, entry.data)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    return True
 
+    # Remove orphaned sensors after entities are created
+    await coordinator.remove_orphaned_sensors()
+
+    return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = hass.data[DOMAIN].get(entry.entry_id)
     if coordinator:
-        await coordinator.async_close()
+        await coordinator._session.close()
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
