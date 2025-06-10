1837b5ae3b3c1794cb47dce066378bf3462ac783
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-06-10 16:12:09 +0200

    Update const.py

--- custom_components/suncloud_monitor/const.py
+++ custom_components/suncloud_monitor/const.py
@@ -4,7 +4,6 @@ DOMAIN = "suncloud_monitor"
 PLATFORMS = ["sensor"]
 CONFIG_STORAGE_FILE = "custom_components/suncloud_monitor/config_storage.yaml"
 CONF_POINTS = "points"
-DEFAULT_SCAN_INTERVAL = 300  # 5 minutes
 
 CONF_APPKEY = "appkey"
 CONF_ACCESS_KEY = "access_key"
